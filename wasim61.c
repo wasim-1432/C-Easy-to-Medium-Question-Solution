3ad36bebf3f393d5701bc93929ad59efd5318caa
wasim-1432 <wasim.ahmad0661@gmail.com>
2024-12-20 16:17:03 +0530

    WAP to take the amount in INR and convert it into USD

---
+++ wasim61.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    float INR;
+    printf("Enter the value of INR\n");
+    scanf("%f",&INR);
+    printf("Conver USD=%f",INR/84.23);
+    printf("\n");
+    return 0;
+}
