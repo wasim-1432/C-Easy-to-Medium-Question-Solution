f6511adbf95a91d23d4a5862f005dbea23fcc017
wasim-1432 <wasim.ahmad0661@gmail.com>
2024-12-20 07:12:49 +0530

    Calculate Circumference of the circle

---
+++ wasim22.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int r;
+    printf("Enter the circumference of the circle\n");
+    scanf("%d",&r);
+    printf("Circumference of the circle is=%f",2*3.14*r);
+    printf("\n");
+    return 0;
+}
