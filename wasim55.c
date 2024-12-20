f55810f90d747171048008519485ef5bbfbd9ba5
wasim-1432 <wasim.ahmad0661@gmail.com>
2024-12-20 16:06:20 +0530

    WAP to input a number from the user and also input a digit.Append a digit in the number.

---
+++ wasim55.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter any numbers\n");
+    scanf("%d",&a);
+    printf("Enter any single digit number\n");
+    scanf("%d",&b);
+    printf("Required answer is=%d",a*10+b);
+    printf("\n");
+    return 0;
+}
