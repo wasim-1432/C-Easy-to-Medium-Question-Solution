aa19a160f86fc4b1164106fbefb194af29d62176
wasim-1432 <wasim.ahmad0661@gmail.com>
2024-12-20 07:21:55 +0530

    Calculate Volume of Cuboid

---
+++ wasim24.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int l,b,h;
+    printf("Enter the length,breath and hight\n");
+    scanf("%d%d%d",&l,&b,&h);
+    printf("Volume of cuboid=%d",l*b*h);
+    printf("\n");
+    return 0;
+}
