12136efb1624a60c56c9e43a72fc514c98ebded2
wasim-1432 <wasim.ahmad0661@gmail.com>
2024-12-21 10:12:08 +0530

    WAP to print Phisic Wallah n times.

---
+++ wasim121.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter the size\n");
+    scanf("%d",&n);
+    while(n)
+    {
+        printf("Physics Wallah\n");
+        n--;
+    }
+    printf("\n");
+    return 0;
+}
