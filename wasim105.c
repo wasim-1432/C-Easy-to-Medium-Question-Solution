de31ef3bbe82fe8349c795769e8d3956534062e5
wasim-1432 <wasim.ahmad0661@gmail.com>
2024-12-21 08:10:10 +0530

    WAP to find the month name.

---
+++ wasim105.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter the month number\n");
+    scanf("%d",&a);
+    if(a==1)
+    {
+        printf("Jan");
+    }
+    else if(a==2)
+    {
+        printf("Feb");
+    }
+    else if(a==3)
+    {
+        printf("March");
+    }
+    else if(a==4)
+    {
+        printf("April");
+    }
+    else if(a==5)
+    {
+        printf("May");
+    }
+    else if(a==6)
+    {
+        printf("June");
+    }
+    else if(a==7)
+    {
+        printf("July");
+    }
+    else if(a==8)
+    {
+        printf("Aug");
+    }
+    else if(a==9)
+    {
+        printf("Sept");
+    }
+    else if(a==10)
+    {
+        printf("Oct");
+    }
+    else if(a==11)
+    {
+        printf("Nov");
+    }
+    else if(a==12)
+    {
+        printf("Dec");
+    }
+    printf("\n");
+    return 0;
+}
