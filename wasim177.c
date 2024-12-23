459c3ffc682eea3cea9b926408ed4b1a5b145c18
wasim-1432 <wasim.ahmad0661@gmail.com>
2024-12-23 07:54:13 +0530

    WAP to print alphabet pattern 1.

---
+++ wasim177.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,k=64;
+    for(i=1;i<=5;i++)
+    {
+        k=65;
+        for(j=1;j<=5;j++)
+        {
+            if(j>=i)
+            {
+                printf("%c",k++);
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    printf("\n");
+    return 0;
+}
